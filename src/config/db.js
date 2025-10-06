454126c9219cb7934576fbf923ed141278dae29c
yasmeenaothman <anayasmeen380@gmail.com>
2025-10-06 09:49:43 +0300

    Update database URL to use remote Render instance

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 // optional: check if connected
 db.on("connect", () => {
